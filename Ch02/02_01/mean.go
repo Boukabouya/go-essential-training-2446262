4ff4eacb08a72c91fc6c259fb56de147b92fbb63
Boukabouya <boukabouyarayane2019@gmail.com>
2024-01-03 21:55:05 +0100

    Numbers and assignments

--- Ch02/02_01/mean.go
+++ Ch02/02_01/mean.go
@@ -23,15 +23,18 @@ func main() {
 	/*
 		x := 1.0
 		y := 2.0
-	*/
-
+		/*	And := is for defining and assigning value to a variable at the same time. And Go is doing type inference*/
 	/*
-		x, y := 1.0, 2.0
-	*/
 
+		/*
+			x, y := 1.0, 2.0
+	*/
+	/*going to use Printf with %v, which is going to print any goal value, and %T, which is going to print the type*/
 	fmt.Printf("x=%v, type of %T\n", x, x)
 	fmt.Printf("y=%v, type of %T\n", y, y)
 
 	mean := (x + y) / 2.0
 	fmt.Printf("result: %v, type of %T\n", mean, mean)
 }
+
+//you cannot have unused variables in Go.
