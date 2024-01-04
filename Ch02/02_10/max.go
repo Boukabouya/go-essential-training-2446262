4889adc0e3a7696d1f92bb1e2f4653d2e8c011a0
Boukabouya <boukabouyarayane2019@gmail.com>
2024-01-04 11:22:49 +0100

    maximal value in a slice

--- Ch02/02_10/max.go
+++ Ch02/02_10/max.go
@@ -7,5 +7,14 @@ import (
 
 func main() {
 	nums := []int{16, 8, 42, 4, 23, 15}
-	fmt.Println(nums)
+	max := nums[0]
+	for _, name := range nums {
+
+		if max > name {
+			continue
+		} else {
+			max = name
+		}
+	}
+	fmt.Println(max)
 }
