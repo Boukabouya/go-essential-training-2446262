9ca87e101f37e46646218c82ce1de888e90b15d1
Boukabouya <boukabouyarayane2019@gmail.com>
2024-01-08 10:00:45 +0100

    create csv tester

--- Ch07/07_03/sqrt_test.go
+++ Ch07/07_03/sqrt_test.go
@@ -2,11 +2,18 @@ package sqrt
 
 import (
 	"testing"
-
+	"encoding/csv"
+	"os"
 	"github.com/stretchr/testify/require"
 )
 
 func TestSimple(t *testing.T) {
+	   // Open the CSV file
+	   file, err := os.Open("./sqrt_cases.csv")
+	   if err != nil {
+		   panic(err)
+	   }
+	   defer file.Close()
 	val, err := Sqrt(2)
 	require.NoError(t, err)
 	require.InDelta(t, 1.414214, val, 0.001)
